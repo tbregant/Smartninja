97e26d5b4885ad7ab17f45eb8de725cc2f21967e
Tomaz Bregant <tbregant@gmail.com>
2018-10-10 22:02:56 +0200

    Update 9.2 Jedilni list

--- Homework/HW9.2_JedilniList/Menu.py
+++ Homework/HW9.2_JedilniList/Menu.py
@@ -1,18 +1,15 @@
 print "Hi, welcome to Menu creator. Lets begin ..."
 
-menu = {}
-
 end = ""
 while end.upper() != "N":
     try:
         name = raw_input("Please enter item name: ")
         price = float(raw_input("Please enter price for " + name + ": "))
-        menu[name] = price
+
+        with open("menu.txt", "a") as menu_file:
+            menu_file.write(name + " " + str(price) + "\r")
+
         end = raw_input("Continue [Y/N]: ")
+
     except ValueError:
         print "Wrong value, try again ... "
-
-with open("menu.txt", "w+") as menu_file:
-    for item in menu:
-        menu_file.write(item + " " + str(menu[item]) + "\r")
-
